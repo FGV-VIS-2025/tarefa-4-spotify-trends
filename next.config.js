e2eefa6c4377dfdf1412870eb7b2f21e9a03a90e
JoãoGabriel <jogabriel433@gmail.com>
2025-04-22 15:33:47 -0300

    Updates

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    assetPrefix: '/tarefa-4-spotify-trends',  
-    basePath: '/tarefa-4-spotify-trends',  
-  }
-  
-  module.exports = nextConfig
-
-
